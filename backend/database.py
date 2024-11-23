c77a722be777f861f01565ab95d7bdef958a1af7
KvsSudhamsh <vishnu.kvs202@gmail.com>
2024-11-23 01:30:41 -0800

    name correction

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgressql://parksmart:parksmart@localhost/parking_app"
+DATABASE_URL = "postgresql://parksmart:parksmart@localhost/parking_app"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit = False, autoflush = False, bing = engine)
