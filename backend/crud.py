98f5822255a94575e40abe0a3f9acbd5e5286741
KvsSudhamsh <vishnu.kvs202@gmail.com>
2024-11-23 02:15:52 -0800

    crud operations

---
+++ backend/crud.py
@@ -0,0 +1,23 @@
+from sqlalchemy.orm import Session
+from models import User, ParkingSpot, Reservation
+
+def create_user(db: Session, username:str, password:str):
+    new_user = User(username = username, password = password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+def get_spots(db: Session):
+    return db.query(ParkingSpot).all()
+
+def reserve_spot(db: Session, user_id:int, spot_id:int):
+    spot = db.query(ParkingSpot).filter(ParkingSpot.id == spot_id).first()
+    if spot and spot.status == "available":
+        spot.status = "reserved"
+        reservation = Reservation(user_id = user_id, spot_id = spot_id)
+        db.add(reservation)
+        db.commit()
+        db.refresh(reservation)
+        return reservation
+    return None
