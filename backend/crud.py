a4e0d315afd039bf2b70ad88019c9107eee28ea3
KvsSudhamsh <vishnu.kvs202@gmail.com>
2024-12-01 23:20:38 -0800

    updated the createUser method

--- backend/crud.py
+++ backend/crud.py
@@ -2,7 +2,10 @@ from sqlalchemy.orm import Session
 from models import User, ParkingSpot, Reservation
 
 def create_user(db: Session, username:str, password:str):
-    new_user = User(username = username, password = password)
+    new_user = User()
+    new_user.Username = username
+    new_user.Password = password
+    # new_user = User(username = username, password = password)
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
